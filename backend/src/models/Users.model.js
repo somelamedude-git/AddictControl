c4ececfd8533bb2d88c99852a3869283b72309ac
samiksha275 <agarwalsamiksha88@gmail.com>
2025-12-14 11:06:00 +0530

    Added addict schema

--- backend/src/models/Users.model.js
+++ backend/src/models/Users.model.js
@@ -2,13 +2,45 @@ const mongoose = require('mongoose')
 
 const userschema = new mongoose.Schema({
     phone: {
-        type: 'String',
-        required: true
+        type: String,
+        required: true,
+        unique: true,
+        index: true
     },
     email: {
-        type: 'String',
+        type: String,
         required: true
     },
+}, {timestamps: true, discriminatorKey: 'role'})
+
+const addictSchema = new mongoose.Schema({
+    sobrierity: {
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor",
+        required: true
+    }, 
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    language: {
+        type: String,
+        enum: ['Hindi', 'English'],
+        default: 'Hindi'
+    }
 }, {timestamps: true})
 
 const User = mongoose.model('User', userschema)
+const Addict = User.discriminator('Addict', addictSchema)
