72f05db078824c25622e4c1f654bfab6824f9e28
somelamedude-git <cozydeveloper843@gmail.com>
2025-12-17 18:45:03 +0530

    a few incomplete elements with the socket and the array setup

--- backend/src/controllers/calls.controller.js
+++ backend/src/controllers/calls.controller.js
@@ -1,5 +1,6 @@
 const { Family } = require('../models/Users.model.js');
 const { Addict } = require('../models/Users.model.js');
+const axios = require('axios');
 
 const request_phone_call = async(req, res)=>{
 	const user_id = req.user._id;
@@ -14,21 +15,40 @@ const request_phone_call = async(req, res)=>{
 				message: "Requested user not found"
 			});
 		}
+
+		const response = await axios.post('link_to_api(add later)', {addict_email}); // add headers later
+		console.log(response.data);
 		return res.status(200).json({
-			success: true,
-			message: "Call and test requested"
+			success:true,
+			message: "Request for call sent"
 		});
 	}
-	catch(error){
-		console.log(error);
+	catch(err){
+		console.log(err);
 		return res.status(500).json({
 			success: false,
-			message: "Some internal server error, try again"
+			message: "Internal server error"
 		});
 	}
 }
 
-const accept_call = async(req, res)=>{
+const addict_portal_call = async(req, res)=>{
+	const {addict_email} = req.body;
+	try{
+		const addict = await Addict.findOne({email:addict_email}); // already verified
+
+		const socket_id = socket_array[addict._id.toString()];
+		io.to(socket_id).emit("incoming_call", {
+			audio: ""
+		});
+		res.json({
+			message: "called"
+		});
+	}
+	catch(error){
+		console.log('screw me');
+	}
+} // to be connected with frontend through socket.on
 
 
 module.exports = {
