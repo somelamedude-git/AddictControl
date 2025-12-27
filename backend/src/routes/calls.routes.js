a3bdae0f80047ff09afe7f9912fc37415c748473
somelamedude-git <cozydeveloper843@gmail.com>
2025-12-28 04:33:59 +0530

    cleanup and call routing completion

--- backend/src/routes/calls.routes.js
+++ backend/src/routes/calls.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { accept_call } = require('../controllers/calls.controller.js');
+const { accept_call, request_phone_call } = require('../controllers/calls.controller.js');
+const {verifyJWT} = require('../middleware/auth.middleware.js');
 
-router.get('/acceptCall', accept_call);
+router.get('/acceptCall',verifyJWT, accept_call);
+router.get('/requestCall', verifyJWT, request_phone_call);
 
 module.exports = router;
