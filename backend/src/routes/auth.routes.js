8ecd65dbc8451062f0cdd04aa79dffc6dff51f94
somelamedude-git <cozydeveloper843@gmail.com>
2025-12-28 15:17:03 +0530

    refresh endpoint added

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {multi_purpose_login} = require('../controllers/login.controller.js');
-
+const { refresh } = require('../utils/tokens.utils.js');
 router.post('/login', multi_purpose_login);
-
+router.post('/refresh', refresh);
 module.exports = router
